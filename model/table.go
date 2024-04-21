9c50382a090c5e6701255c4380c3b91cc7fe2d1b
lhdht <15229684090@163.com>
2024-04-21 17:24:25 +0800

    fix: 移除无效钩子

--- model/table.go
+++ model/table.go
@@ -5,24 +5,9 @@ import (
 	"time"
 )
 
-type MongoTableModel struct {
+type MongoTableEntity struct {
 	ID        primitive.ObjectID `json:"id" bson:"_id"`
 	CreatedAt time.Time          `json:"created_at" bson:"created_at"`
 	UpdatedAt time.Time          `json:"updated_at" bson:"updated_at"`
 	DeletedAt time.Time          `json:"deleted_at" bson:"deleted_at"`
-}
-
-func (mm *MongoTableModel) BeforeInset() {
-	mm.ID = primitive.NewObjectID()
-	mm.CreatedAt = time.Now().Local()
-	mm.UpdatedAt = time.Now().Local()
-}
-
-func (mm *MongoTableModel) BeforeUpdate() {
-	mm.UpdatedAt = time.Now().Local()
-}
-
-func (mm *MongoTableModel) BeforeDelete() {
-	mm.UpdatedAt = time.Now().Local()
-	mm.DeletedAt = time.Now().Local()
 }
