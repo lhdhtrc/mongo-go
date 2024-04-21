af4c793eebed5dbdc351133efa4f1fbf9cfec374
lhdht <15229684090@163.com>
2024-04-21 17:30:33 +0800

    fix: 修改名称

--- model/table.go
+++ model/table.go
@@ -5,9 +5,24 @@ import (
 	"time"
 )
 
-type MongoTableEntity struct {
+type MongoTableModeEntity struct {
 	ID        primitive.ObjectID `json:"id" bson:"_id"`
 	CreatedAt time.Time          `json:"created_at" bson:"created_at"`
 	UpdatedAt time.Time          `json:"updated_at" bson:"updated_at"`
 	DeletedAt time.Time          `json:"deleted_at" bson:"deleted_at"`
+}
+
+func (mm *MongoTableModeEntity) BeforeInset() {
+	mm.ID = primitive.NewObjectID()
+	mm.CreatedAt = time.Now().Local()
+	mm.UpdatedAt = time.Now().Local()
+}
+
+func (mm *MongoTableModeEntity) BeforeUpdate() {
+	mm.UpdatedAt = time.Now().Local()
+}
+
+func (mm *MongoTableModeEntity) BeforeDelete() {
+	mm.UpdatedAt = time.Now().Local()
+	mm.DeletedAt = time.Now().Local()
 }
