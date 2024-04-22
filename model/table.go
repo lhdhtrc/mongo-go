f6265546f83d7e964ba60ddf070d16b87b00a2fe
lhdhtrc <54212738+lhdhtrc@users.noreply.github.com>
2024-04-22 14:27:30 +0800

    Update table.go

--- model/table.go
+++ model/table.go
@@ -5,24 +5,24 @@ import (
 	"time"
 )
 
-type MongoTableModeEntity struct {
+type MongoTableEntity struct {
 	ID        primitive.ObjectID `json:"id" bson:"_id"`
 	CreatedAt time.Time          `json:"created_at" bson:"created_at"`
 	UpdatedAt time.Time          `json:"updated_at" bson:"updated_at"`
 	DeletedAt time.Time          `json:"deleted_at" bson:"deleted_at"`
 }
 
-func (mm *MongoTableModeEntity) BeforeInset() {
+func (mm *MongoTableEntity) BeforeInset() {
 	mm.ID = primitive.NewObjectID()
 	mm.CreatedAt = time.Now().Local()
 	mm.UpdatedAt = time.Now().Local()
 }
 
-func (mm *MongoTableModeEntity) BeforeUpdate() {
+func (mm *MongoTableEntity) BeforeUpdate() {
 	mm.UpdatedAt = time.Now().Local()
 }
 
-func (mm *MongoTableModeEntity) BeforeDelete() {
+func (mm *MongoTableEntity) BeforeDelete() {
 	mm.UpdatedAt = time.Now().Local()
 	mm.DeletedAt = time.Now().Local()
 }
