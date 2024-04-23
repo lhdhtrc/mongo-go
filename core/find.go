2d1d0ad1d4625e0072dc2d7b2e4c865e97f0ca5c
lhdht <15229684090@163.com>
2024-04-23 10:36:30 +0800

    feature: 分页查询

--- core/find.go
+++ core/find.go
@@ -1,1 +1,13 @@
 package core
+
+import "go.mongodb.org/mongo-driver/mongo/options"
+
+func Paging(page uint, pageSize uint, option *options.FindOptions) {
+	if page != 0 {
+		if pageSize > 50 {
+			pageSize = 50
+		}
+		option.SetLimit(int64(pageSize))
+		option.SetSkip(int64((page - 1) * pageSize))
+	}
+}
