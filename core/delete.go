d011c13dd331d6653ba416104d59c0c6cf8b7412
lhdht <15229684090@163.com>
2024-04-23 11:01:52 +0800

    fix: strid转换object_id

--- core/delete.go
+++ core/delete.go
@@ -10,14 +10,14 @@ import (
 func Delete(ctx context.Context, collection *mongo.Collection, id string) {
 	_, _ = collection.DeleteOne(ctx, bson.E{
 		Key:   "_id",
-		Value: id,
+		Value: StrIdToObjectId([]string{id})[0],
 	})
 }
 
 func DeleteMany(ctx context.Context, collection *mongo.Collection, ids []string) {
 	_, _ = collection.DeleteMany(ctx, bson.E{Key: "_id", Value: bson.E{
 		Key:   "$in",
-		Value: ids,
+		Value: StrIdToObjectId(ids),
 	}})
 }
 
@@ -25,7 +25,7 @@ func SoftDelete(ctx context.Context, collection *mongo.Collection, id string) {
 	timer := time.Now().Local()
 	_, _ = collection.UpdateOne(ctx, bson.E{
 		Key:   "_id",
-		Value: id,
+		Value: StrIdToObjectId([]string{id})[0],
 	}, bson.D{
 		{"updated_at", timer},
 		{"deleted_at", timer},
@@ -36,7 +36,7 @@ func SoftDeleteMany(ctx context.Context, collection *mongo.Collection, ids []str
 	timer := time.Now().Local()
 	_, _ = collection.UpdateMany(ctx, bson.E{
 		Key:   "_id",
-		Value: bson.E{Key: "$in", Value: ids},
+		Value: bson.E{Key: "$in", Value: StrIdToObjectId(ids)},
 	}, bson.D{
 		{"updated_at", timer},
 		{"deleted_at", timer},
