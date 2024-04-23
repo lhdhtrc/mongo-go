7f896eba2e3244b9d7191226772b667310a81f2b
lhdht <15229684090@163.com>
2024-04-23 09:44:08 +0800

    feature: 硬删除软删除

--- core/delete.go
+++ core/delete.go
@@ -1,1 +1,44 @@
 package core
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"time"
+)
+
+func Delete(ctx context.Context, collection *mongo.Collection, id string) {
+	_, _ = collection.DeleteOne(ctx, bson.E{
+		Key:   "_id",
+		Value: id,
+	})
+}
+
+func DeleteMany(ctx context.Context, collection *mongo.Collection, ids interface{}) {
+	_, _ = collection.DeleteMany(ctx, bson.E{Key: "_id", Value: bson.E{
+		Key:   "$in",
+		Value: ids,
+	}})
+}
+
+func SoftDelete(ctx context.Context, collection *mongo.Collection, id string) {
+	timer := time.Now().Local()
+	_, _ = collection.UpdateOne(ctx, bson.E{
+		Key:   "_id",
+		Value: id,
+	}, bson.D{
+		{"updated_at", timer},
+		{"deleted_at", timer},
+	})
+}
+
+func SoftDeleteMany(ctx context.Context, collection *mongo.Collection, ids interface{}) {
+	timer := time.Now().Local()
+	_, _ = collection.UpdateMany(ctx, bson.E{
+		Key:   "_id",
+		Value: bson.E{Key: "$in", Value: ids},
+	}, bson.D{
+		{"updated_at", timer},
+		{"deleted_at", timer},
+	})
+}
