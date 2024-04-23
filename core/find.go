2a96e117295028051ece0f856fd2cfaed2a83fd5
lhdht <15229684090@163.com>
2024-04-24 07:56:55 +0800

    fix: 废弃时间筛选

--- core/find.go
+++ core/find.go
@@ -1,7 +1,6 @@
 package core
 
 import (
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
@@ -12,17 +11,5 @@ func Paging(page uint, pageSize uint, option *options.FindOptions) {
 		}
 		option.SetLimit(int64(pageSize))
 		option.SetSkip(int64((page - 1) * pageSize))
-	}
-}
-
-func Timer(start string, end string, d bson.D) {
-	if len(start) != 0 && len(end) != 0 {
-		d = append(d, bson.E{
-			Key: "created_at",
-			Value: bson.D{
-				{"$gte", start},
-				{"$lte", end},
-			},
-		})
 	}
 }
