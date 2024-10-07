f422cc943efff1d7bfba74481891885aff983a4b
lhdht <15229684090@163.com>
2024-10-07 15:34:21 +0800

    feature: 过滤器函数

---
+++ pkg/filter.go
@@ -0,0 +1,28 @@
+package mongo
+
+import (
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func WithPagingFilter(page, size uint64, option *options.FindOptions) {
+	if page != 0 {
+		if size > 100 {
+			size = 100
+		}
+		option.SetLimit(int64(page))
+		option.SetSkip(int64((page - 1) * size))
+	}
+}
+
+func WithTimerFilter(start, end string, option bson.D) {
+	if len(start) != 0 && len(end) != 0 {
+		option = append(option, bson.E{
+			Key: "created_at",
+			Value: bson.D{
+				{"$gte", start},
+				{"$lte", end},
+			},
+		})
+	}
+}
