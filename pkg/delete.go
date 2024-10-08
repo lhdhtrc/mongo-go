89e4348d350a465732a245f24375f8709da54ed0
lhdht <15229684090@163.com>
2024-10-08 20:25:58 +0800

    fix: 修复无法正常删除问题

--- pkg/delete.go
+++ pkg/delete.go
@@ -9,37 +9,37 @@ import (
 )
 
 func Delete(ctx context.Context, collection *mongo.Collection, id primitive.ObjectID) {
-	_, _ = collection.DeleteOne(ctx, bson.E{
-		Key:   "_id",
-		Value: id,
+	_, _ = collection.DeleteOne(ctx, bson.D{
+		{"_id", id},
 	})
 }
 
 func DeleteMany(ctx context.Context, collection *mongo.Collection, ids []string) {
-	_, _ = collection.DeleteMany(ctx, bson.E{Key: "_id", Value: bson.E{
-		Key:   "$in",
-		Value: StrIdToObjectId(ids),
-	}})
+	_, _ = collection.DeleteMany(ctx, bson.D{
+		{"_id", bson.E{Key: "$in", Value: StrIdToObjectId(ids)}},
+	})
 }
 
 func SoftDelete(ctx context.Context, collection *mongo.Collection, id primitive.ObjectID) {
 	timer := time.Now().UTC()
-	_, _ = collection.UpdateOne(ctx, bson.E{
-		Key:   "_id",
-		Value: id,
+	_, _ = collection.UpdateOne(ctx, bson.D{
+		{"_id", id},
 	}, bson.D{
-		{"updated_at", timer},
-		{"deleted_at", timer},
+		{"$set", bson.D{
+			{"updated_at", timer},
+			{"deleted_at", timer},
+		}},
 	})
 }
 
 func SoftDeleteMany(ctx context.Context, collection *mongo.Collection, ids []string) {
 	timer := time.Now().UTC()
-	_, _ = collection.UpdateMany(ctx, bson.E{
-		Key:   "_id",
-		Value: bson.E{Key: "$in", Value: StrIdToObjectId(ids)},
+	_, _ = collection.UpdateMany(ctx, bson.D{
+		{"_id", bson.E{Key: "$in", Value: StrIdToObjectId(ids)}},
 	}, bson.D{
-		{"updated_at", timer},
-		{"deleted_at", timer},
+		{"$set", bson.D{
+			{"updated_at", timer},
+			{"deleted_at", timer},
+		}},
 	})
 }
