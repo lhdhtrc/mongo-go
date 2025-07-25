bdbab0e4b7698205ae3e30db930b85ae33055cbd
lhdht <15229684090@163.com>
2025-07-25 09:06:56 +0800

    fix: 修改耗时记录

--- pkg/internal/logger.go
+++ pkg/internal/logger.go
@@ -121,7 +121,7 @@ func (l *logger) handleLog(ctx context.Context, level LogLevel, smt, result stri
 			"Database":  l.Database,
 			"Statement": smt,
 			"Result":    result,
-			"Duration":  elapsed.Milliseconds(),
+			"Duration":  elapsed.String(),
 			"Level":     level,
 			"Type":      LogTypeMongo,
 		}
